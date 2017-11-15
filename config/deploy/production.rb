7d601e463861a480828e78922750549918550589
Tim <s6215s5216@gmail.com>
2017-11-15 12:22:21 -0500

    deploy through master

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-set :branch, "linux"
+set :branch, "master"
   server "69.164.220.158", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
