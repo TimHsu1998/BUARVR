b3b763682bd4ea61a4fccf193bc41fd189de570a
Tim <s6215s5216@gmail.com>
2017-11-24 16:50:11 -0500

    edit subscription

--- app/controllers/emailinglists_controller.rb
+++ app/controllers/emailinglists_controller.rb
@@ -8,22 +8,22 @@ class EmailinglistsController < ApplicationController
   def create
     @emailinglist = Emailinglist.new(emailinglist_params)
     if @emailinglist.save
+      name = @emailinglist.name
+      email = @emailinglist.email
+      # Authenticate a session with your Service Account
+      session = GoogleDrive::Session.from_service_account_key("client_secret.json")
+
+      # Get the spreadsheet by its title
+      spreadsheet = session.spreadsheet_by_title("2017 member list")
+      # Get the first worksheet
+      worksheet = spreadsheet.worksheets.fourth
+      # Print out the first 6 columns of each row
+      worksheet.insert_rows(2, [[name, email]])
+      worksheet.save
       redirect_to root_path
     else
       render :new
     end
-    name = @emailinglist.name
-    email = @emailinglist.email
-    # Authenticate a session with your Service Account
-    session = GoogleDrive::Session.from_service_account_key("client_secret.json")
-
-    # Get the spreadsheet by its title
-    spreadsheet = session.spreadsheet_by_title("2017 member list")
-    # Get the first worksheet
-    worksheet = spreadsheet.worksheets.fourth
-    # Print out the first 6 columns of each row
-    worksheet.insert_rows(2, [[name, email]])
-    worksheet.save
   end
 
   private
