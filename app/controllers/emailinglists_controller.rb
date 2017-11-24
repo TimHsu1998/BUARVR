c4170946b793bb7f323717147bee86038aa98a54
Tim <s6215s5216@gmail.com>
2017-11-24 16:39:30 -0500

    get google API for sheet

--- app/controllers/emailinglists_controller.rb
+++ app/controllers/emailinglists_controller.rb
@@ -1,3 +1,5 @@
+require 'bundler'
+Bundler.require
 class EmailinglistsController < ApplicationController
   def new
     @emailinglist = Emailinglist.new
@@ -10,6 +12,18 @@ class EmailinglistsController < ApplicationController
     else
       render :new
     end
+    name = @emailinglist.name
+    email = @emailinglist.email
+    # Authenticate a session with your Service Account
+    session = GoogleDrive::Session.from_service_account_key("client_secret.json")
+
+    # Get the spreadsheet by its title
+    spreadsheet = session.spreadsheet_by_title("2017 member list")
+    # Get the first worksheet
+    worksheet = spreadsheet.worksheets.fourth
+    # Print out the first 6 columns of each row
+    worksheet.insert_rows(2, [[name, email]])
+    worksheet.save
   end
 
   private
