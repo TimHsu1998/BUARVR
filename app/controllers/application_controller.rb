2521d872227c2d4963db664750c74173c79e6385
Tim <s6215s5216@gmail.com>
2017-12-28 12:38:59 -0800

    fix the future time bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,16 +9,15 @@ class ApplicationController < ActionController::Base
   end
 
   def in_future(date)
-    if date.year > Time.now.year
+    if date.year < Time.now.year
+      return false
+    elsif date.month < Time.now.month
+      return false
+    elsif date.day < Time.now.day
+      return false
+    else
       return true
     end
-    if date.month > Time.now.month
-      return true
-    end
-    if date.day >= Time.now.day
-      return true
-    end
-    return false
   end
 
 end
